43f9dd1299d449345141db15a5d4315c411bdc1e
AlexanderPhan <42187090+AlexanderPhan@users.noreply.github.com>
2018-11-26 14:57:00 -0600

    index.js

---
+++ Lib/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
+INSERT INTO burgers (burger_name) VALUES ('Plain Burger');
+INSERT INTO burgers (burger_name) VALUES ('Texas Burger');
+INSERT INTO burgers (burger_name) VALUES ('Wisconsin Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Chicken Burger', true);
